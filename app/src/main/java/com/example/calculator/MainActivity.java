5c779723595997e386d508dd17c77b02b2a28906
sapir-parienti <sapir.pa@sisma.org.il>
2024-03-31 19:31:22 +0300

    2

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,19 +2,44 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public int sum=0;
+    private TextView display;
+    private Button plus;
+    private Button minus;
+    private Button divide;
+    private Button multiply;
+    private Button ac;
+    private Button equal;
+    private Button credits;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        display=findViewById(R.id.display);
+        plus=findViewById(R.id.plus);
+        minus=findViewById(R.id.minus);
+        divide=findViewById(R.id.divide);
+        multiply=findViewById(R.id.multiply);
+        ac=findViewById(R.id.ac);
+        equal=findViewById(R.id.equal);
+        credits=findViewById(R.id.credits);
+    }
+    public void credits (View view) {
         Intent si = new Intent( this, credits.class);
         si.putExtra("last number:",sum);
         startActivity(si);
-        finish();
+
     }
 }
